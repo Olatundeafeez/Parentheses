a20c9a414b9e47ecbbf02afd3f39149ce4d70172
Olatundeafeez <olatundefolorunshoafeez@gmail.com>
2024-07-16 21:11:16 +0100

    first parentheses

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.WriteLine(IsValid("())(("));
+Console.WriteLine(IsValid("(afeez())"));
+Console.WriteLine(IsValid("(((((())"));
+bool IsValid(string input)
+{
+    int output = 0; 
+    foreach(char c in input)
+    {
+        if (c == '(')
+        {
+            output++;        
+        }
+        else if(c == ')') 
+        {
+        output --;
+        if (output < 0)
+          {
+
+          return false;
+          }
+           
+        }
+      
+    }
+    return output == 0;
+}
